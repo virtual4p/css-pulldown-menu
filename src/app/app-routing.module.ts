f11b8b5eb92b170ccf2845903faee4837a5626e5
t.keller <virtual4p@gmail.com>
2019-04-07 09:19:24 +0200

    rc
    Signed-off-by: t.keller <virtual4p@gmail.com>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { WelcomeComponent} from './welcome/welcome.component';
+import { DevelopementComponent } from './developement/developement.component';
+import { CloudComponent} from './cloud/cloud.component';
+import { ConsultationComponent } from './consultation/consultation.component';
+import { CompanyComponent } from './company/company.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'welcome', component: WelcomeComponent },
+  { path: 'developement', component:  DevelopementComponent },
+  { path: 'cloud', component:  CloudComponent },
+  { path: 'consultation', component:  ConsultationComponent },
+  { path: 'company', component:  CompanyComponent },
+  { path: 'employees', component:  EmployeesComponent },
+  { path: 'references', component:  ReferencesComponent },
+  { path: 'contact', component:  ContactComponent },
   { path: '',
     redirectTo: '/welcome',
     pathMatch: 'full'
